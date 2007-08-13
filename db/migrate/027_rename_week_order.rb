3b43292a994214f05e854aa7690c803123e2177c
jkay <jkay@723c2f8d-5c36-0410-a5aa-ddc95a8ab578>
2007-08-13 21:30:13 +0000

    fixed some buggys
    git-svn-id: svn+ssh://208.53.44.52/usr/local/svn/broncodashboard@19 723c2f8d-5c36-0410-a5aa-ddc95a8ab578

--- db/migrate/027_rename_week_order.rb
+++ db/migrate/027_rename_week_order.rb
@@ -1,7 +1,7 @@
 class RenameWeekOrder < ActiveRecord::Migration
   def self.up
     #rename_column :weeks, :order, :week_order
-    #ALTER TABLE `broncodashboard_development`.`weeks` CHANGE COLUMN `order` `week_order` INTEGER DEFAULT NULL;
+    #ALTER TABLE `broncodashboard_production`.`weeks` CHANGE COLUMN `order` `week_order` INTEGER DEFAULT NULL;
   end
 
   def self.down
