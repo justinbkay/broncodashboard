786ceaa101175b9d5013bd676b7158566025723f
jkay <jkay@723c2f8d-5c36-0410-a5aa-ddc95a8ab578>
2007-10-07 01:36:37 +0000

    adding sweeper
    git-svn-id: svn+ssh://208.53.44.52/usr/local/svn/broncodashboard@42 723c2f8d-5c36-0410-a5aa-ddc95a8ab578

--- config/environment.rb
+++ config/environment.rb
@@ -42,6 +42,7 @@ Rails::Initializer.run do |config|
   # config.active_record.default_timezone = :utc
   
   # See Rails::Configuration for more options
+  config.load_paths += %W( #{RAILS_ROOT}/app/sweepers )
 end
 
 # Add new inflection rules using the following format 
