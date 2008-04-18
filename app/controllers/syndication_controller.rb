20e6d2f1589371efe42a92985003d3f1d19c10ab
jkay <jkay@723c2f8d-5c36-0410-a5aa-ddc95a8ab578>
2008-04-18 23:22:37 +0000

    updating for 2008 season
    git-svn-id: svn+ssh://208.53.44.52/usr/local/svn/broncodashboard@61 723c2f8d-5c36-0410-a5aa-ddc95a8ab578

--- app/controllers/syndication_controller.rb
+++ app/controllers/syndication_controller.rb
@@ -5,7 +5,7 @@ class SyndicationController < ApplicationController
     @team = Team.find(1)
     @tz = get_tz
     @schedule = Game.find(:all, 
-                          :conditions => ['home_team_id = 1 or visitor_team_id = 1 AND season_id=?',Game::SEASON],
+                          :conditions => ['home_team_id = 1 AND season_id=? or visitor_team_id = 1 AND season_id=?',Game::SEASON,Game::SEASON],
                           :include => [:week], 
                           :order => 'game_time')
     @headers["Content-Type"] = "application/rss+xml"
