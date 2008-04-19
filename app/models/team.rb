eef6af0400af8876f8473b5cf9c3052cfbbc7f07
jkay <jkay@723c2f8d-5c36-0410-a5aa-ddc95a8ab578>
2008-04-19 21:26:49 +0000

    adding season logic and new pic of widget v2
    git-svn-id: svn+ssh://208.53.44.52/usr/local/svn/broncodashboard@82 723c2f8d-5c36-0410-a5aa-ddc95a8ab578

--- app/models/team.rb
+++ app/models/team.rb
@@ -11,68 +11,70 @@ class Team < ActiveRecord::Base
   end
   
   def home_wins
-    Game.count(:conditions => ['home_team_id = ? AND s.id=1 AND complete=1 AND home_score > visitor_score', self.id],
+    Game.count(:conditions => ['home_team_id = ? AND s.id=? AND complete=1 AND home_score > visitor_score', self.id, Game::SEASON],
                :joins => 'as g inner join weeks w on g.week_id=w.id inner join seasons s on w.season_id=s.id')
   end
   
   def home_loses
-    Game.count(:conditions => ['home_team_id = ? AND s.id=1 AND complete=1 AND home_score < visitor_score', self.id],
+    Game.count(:conditions => ['home_team_id = ? AND s.id=? AND complete=1 AND home_score < visitor_score', self.id, Game::SEASON],
                :joins => 'as g inner join weeks w on g.week_id=w.id inner join seasons s on w.season_id=s.id')
   end
   
   def away_loses
-    Game.count(:conditions => ['visitor_team_id = ? AND s.id=1 AND complete=1 AND home_score > visitor_score', self.id],
+    Game.count(:conditions => ['visitor_team_id = ? AND s.id=? AND complete=1 AND home_score > visitor_score', self.id, Game::SEASON],
                :joins => 'as g inner join weeks w on g.week_id=w.id inner join seasons s on w.season_id=s.id')
   end
   
   def away_wins
-    Game.count(:conditions => ['visitor_team_id = ? AND s.id=1 AND complete=1 AND home_score < visitor_score', self.id],
+    Game.count(:conditions => ['visitor_team_id = ? AND s.id=? AND complete=1 AND home_score < visitor_score', self.id, Game::SEASON],
                :joins => 'as g inner join weeks w on g.week_id=w.id inner join seasons s on w.season_id=s.id')
   end
   
   def conference_wins
-    Game.count(:conditions => ['visitor_team_id=? and visitor_team.conference_id=home_team.conference_id and games.complete=1 and games.visitor_score > games.home_score
-  or home_team_id=? and home_team.conference_id=visitor_team.conference_id and games.complete=1 and games.home_score > games.visitor_score', self.id, self.id],
+    Game.count(:conditions => ['visitor_team_id=? and visitor_team.conference_id=home_team.conference_id and games.complete=1 and games.visitor_score > games.home_score AND s.id=?
+  or s.id=? AND home_team_id=? and home_team.conference_id=visitor_team.conference_id and games.complete=1 and games.home_score > games.visitor_score', self.id, Game::SEASON self.id, Game::SEASON],
                :joins => 'inner join teams home_team on games.home_team_id=home_team.id 
-               inner join teams visitor_team on visitor_team.id=games.visitor_team_id')
+               inner join teams visitor_team on visitor_team.id=games.visitor_team_id inner join weeks w on w.id=games.week_id inner join seasons s on s.id=weeks.season_id')
   end
   
   def conference_loses
-    Game.count(:conditions => ['visitor_team_id=? and visitor_team.conference_id=home_team.conference_id and games.complete=1 and games.visitor_score < games.home_score
-  or home_team_id=? and home_team.conference_id=visitor_team.conference_id and games.complete=1 and games.home_score < games.visitor_score', self.id, self.id],
+    Game.count(:conditions => ['visitor_team_id=? and visitor_team.conference_id=home_team.conference_id and games.complete=1 and games.visitor_score < games.home_score AND s.id=?
+  or s.id=? AND home_team_id=? and home_team.conference_id=visitor_team.conference_id and games.complete=1 and games.home_score < games.visitor_score', self.id, Game::SEASON, self.id, Game::SEASON],
                :joins => 'inner join teams home_team on games.home_team_id=home_team.id 
-               inner join teams visitor_team on visitor_team.id=games.visitor_team_id')
+               inner join teams visitor_team on visitor_team.id=games.visitor_team_id inner join weeks w on w.id=games.week_id inner join seasons s on s.id=w.season_id')
   end
   
   def ranked_wins
-    Game.count(:conditions => ['visitor_team_id=? and games.home_team_ap_rank > 0 and games.complete=1 and games.visitor_score > games.home_score
-  OR home_team_id=? and games.visitor_team_ap_rank > 0  and games.complete=1 and games.home_score > games.visitor_score', self.id, self.id])
+    Game.count(:conditions => ['visitor_team_id=? and games.home_team_ap_rank > 0 and games.complete=1 and games.visitor_score > games.home_score AND s.id=?
+  OR home_team_id=? and games.visitor_team_ap_rank > 0  and games.complete=1 and games.home_score > games.visitor_score AND s.id=?', self.id, Game::SEASON,self.id,Game::SEASON],
+               :include => {:week => :season})
   end
   
   def ranked_loses
-    Game.count(:conditions => ['visitor_team_id=? and games.home_team_ap_rank > 0 and games.complete=1 and games.visitor_score < games.home_score
-  OR home_team_id=? and games.visitor_team_ap_rank > 0 and games.complete=1 and games.home_score < games.visitor_score', self.id, self.id])
+    Game.count(:conditions => ['visitor_team_id=? and games.home_team_ap_rank > 0 and games.complete=1 and games.visitor_score < games.home_score AND s.id=?
+  OR home_team_id=? and games.visitor_team_ap_rank > 0 and games.complete=1 and games.home_score < games.visitor_score AND s.id=?', self.id, Game::SEASON,self.id,Game::SEASON],
+               :include => {:week => :season})
     
   end
   
   def home_score_total
-    Game.sum('home_score', :conditions => ['home_team_id=? and complete=1',self.id]) || 0
+    Game.sum('home_score', :conditions => ['home_team_id=? and complete=1 AND season_id=?',self.id,Game::SEASON], :include => {:week => :season}) || 0
   end
   
   def visitor_score_total
-    Game.sum('visitor_score', :conditions => ['visitor_team_id = ? AND complete=1', self.id]) || 0
+    Game.sum('visitor_score', :conditions => ['visitor_team_id = ? AND complete=1 AND season_id=?', self.id,Game::SEASON], :include => {:week => :season}) || 0
   end
   
   def game_count
-    Game.count(:conditions => ['home_team_id=? AND complete=1 OR visitor_team_id=? AND complete=1',self.id,self.id])
+    Game.count(:conditions => ['home_team_id=? AND complete=1 AND season_id=? OR visitor_team_id=? AND complete=1 AND season_id=?',self.id,Game::SEASON,self.id,Game::SEASON], :include => {:week => :season})
   end
   
   def home_points_allowed
-    Game.sum('visitor_score', :conditions => ['home_team_id=? and complete=1',self.id]) || 0
+    Game.sum('visitor_score', :conditions => ['home_team_id=? and complete=1 AND season_id=?',self.id,Game::SEASON], :include => {:week => :season}) || 0
   end
   
   def visitor_points_allowed
-    Game.sum('home_score', :conditions => ['visitor_team_id=? and complete=1',self.id]) || 0
+    Game.sum('home_score', :conditions => ['visitor_team_id=? and complete=1 AND season_id=?',self.id, Game::SEASON], :include => {:week => :season}) || 0
   end
   
   def average_allowed
