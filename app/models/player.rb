30b243f49de0b980a4e34eb845f3c0d5580a5391
Justin Kay <jkay@Lhotse.local>
2009-08-10 18:09:50 -0600

    ordering the player roster

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
-  named_scope :active, :conditions => {:active => true}
+  named_scope :active, :conditions => {:active => true}, :order => :number
   
   def name
     self.first_name + ' ' + self.last_name
