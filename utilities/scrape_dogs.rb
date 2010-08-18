85839e20166b1eb3fdebd313e10d45810d74416c
Justin B Kay <jkay@nwrecc.org>
2010-08-18 17:15:12 -0600

    adding fresno scrape

---
+++ utilities/scrape_dogs.rb
@@ -0,0 +1,56 @@
+require 'rubygems'
+require 'open-uri'
+require 'hpricot'
+require 'active_record'
+
+class Bdb_production < ActiveRecord::Base
+ self.abstract_class = true
+ establish_connection(
+ 	:adapter => "mysql",
+ 	:host     => "localhost",
+ 	:username => "root",
+ 	:password => "nwrecc01",
+ 	:database => "broncodashboard_production"
+ )
+end
+class Player < Bdb_production
+  validates_presence_of :number, :on => :create, :message => "can't be blank"
+end
+
+#@players = Player.all(:conditions => 'team_id=')
+
+#@players.each do |p|
+#  p.destroy
+#end
+
+aruba = Hpricot(open("http://www.gobulldogs.com/sports/m-footbl/mtt/fres-m-footbl-mtt.html"))
+#aruba.search("//table[@class='default_dgrd roster_dgrd']//tr").each_with_index do |game, index|
+aruba.search("//table//tr//td//font//b")[1].parent.parent.parent.parent.children_of_type('tr').each_with_index do |game, index|
+ next if index == 0
+ results = game.search("//td")
+ a = Player.create(:number => results[0].inner_html.strip,
+	               :first_name => results[2].inner_html.scan(/.*>(.*)<.*/)[0][0].strip.split[0],
+	               :last_name => results[2].inner_html.scan(/.*>(.*)<.*/)[0][0].strip.split[1],
+	               :position => results[3].inner_html.strip,
+	               :height => results[4].inner_html.scan(/.*>(.*)<.*/)[0][0].strip,
+	               :weight => results[5].inner_html.strip,
+	               :year => results[6].inner_html.strip,
+	               :hometown => results[7].inner_html.strip.split("/")[0].strip,
+	               :team_id => 13,
+	               :previous_school =>
+results[7].inner_html.strip.split("/")[1].strip) 
+	
+ puts results[0].inner_html.strip + ' :: ' +
+            #number
+      results[2].inner_html.scan(/.*>(.*)<.*/)[0][0].strip + ' :: ' +
+            #name
+      results[3].inner_html.strip + ' :: ' +
+            #position
+      results[4].inner_html.scan(/.*>(.*)<.*/)[0][0].strip + ' :: ' +
+            #height
+      results[5].inner_html.strip + ' :: ' +
+            #weight
+      results[6].inner_html.strip + ' :: ' +
+            #year
+      results[7].inner_html.strip unless results[0].inner_html.strip== ''         #hometown/hs
+end
