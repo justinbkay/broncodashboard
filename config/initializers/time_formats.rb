7ce4b559576978b2380c5c2db2f8fe5e524c58c2
Justin <justinbkay@gmail.com>
2013-03-09 14:28:54 -0700

    adding back the timestamp formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,4 @@
 Time::DATE_FORMATS[:time] = "%I:%M %p"
-Time::DATE_FORMATS[:timestamp] = "%A, %b"
+Time::DATE_FORMATS[:day] = "%a, %b %d"
+Time::DATE_FORMATS[:timestamp] = "%A, %b %d %Y - %I:%M %p"
+Time::DATE_FORMATS[:short_timestamp] = "%m/%d/%Y - %I:%M %p"
